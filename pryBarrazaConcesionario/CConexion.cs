9bf1ec6a4194bcce7e944f7ede192f1d8c951484
Alumno <Alumno@PC133>
2026-04-23 10:16:11 -0300

    Agregar archivos de proyecto.

---
+++ pryBarrazaConcesionario/CConexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryBarrazaConcesionario
+{
+    internal class CConexion
+    {
+        public OleDbConnection conectorBaseDatos;
+        OleDbConnection comandosBaseDatos;
+        public string estadoConexion = "sin conexion";
+        //metodos para abrir la base
+        public void ConectarBaseDatos()
+        {
+            try
+            {
+                conectorBaseDatos = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=concesionaria.accdb;Persist Security Info=False;");
+                conectorBaseDatos.Open();
+                estadoConexion = conectorBaseDatos.State.ToString();
+            }
+            catch (Exception error)
+            {
+                estadoConexion = "error: " + error.Message;
+                throw;
+            }
+        }
+    }
+}
---
+++ pryBarrazaConcesionario/frmPrincipal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryBarrazaConcesionario
+{
+    public partial class frmPrincipal : Form
+    {   
+        private CConexion miConexion;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            CConexion objetoConeccionBaseDatos = new CConexion();
+            objetoConeccionBaseDatos.ConectarBaseDatos();
+            string query = "SELECT * FROM Vehiculo";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query,objetoConeccionBaseDatos.conectorBaseDatos);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+        }
+    }
+}
